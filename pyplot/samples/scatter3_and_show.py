6c8b39fd936fc29b6071ce88eebc07e6c49286f2
Aymen MOUSSI <aymen.moussi.ext@mappy.com>
2019-08-29 13:04:22 +0200

    monty hall prob pyplot exploration

---
+++ pyplot/samples/scatter3_and_show.py
@@ -0,0 +1,31 @@
+from datascience_monty_hall import play
+from datascience_monty_hall import Strategie
+
+import matplotlib.pyplot as plt
+
+
+'''
+scatter is like plot but don't link the points to each other
+'''
+def main():
+
+    gains_changer = []
+    gains_garder = []
+
+    samples = [1000, 10000, 20000, 50000, 80000, 100000]
+
+    for tours in samples:
+        print("En changeant de porte, le joueur a gagné {} sur 10000 parties."
+              .format(sum(play(Strategie.CHANGER, tours))))
+        print("En gardant son choix initial, le joueur a gagné {} sur 10000 parties."
+              .format(sum(play(Strategie.GARDER, tours))))
+        gains_garder.append(sum(play(Strategie.GARDER, tours))/tours)
+        gains_changer.append(sum(play(Strategie.CHANGER, tours))/tours)
+
+    plt.scatter(samples, gains_changer)
+    plt.scatter(samples, gains_garder)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
